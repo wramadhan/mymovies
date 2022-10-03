b8ba83e8e13793ef4b1e30e76d168365ad03ffbc
wramadhan <wahyuramadhan0549@gmail.com>
2022-10-03 18:56:14 +0700

    sedang dalam tahap pembangunan

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,67 @@
-import { MdOutlineLocalMovies } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { Icon } from "@iconify/react";
+import { useState } from "react";
 
-const NavBar = ({ theme }) => {
+const NavBar = () => {
+  const [searchBox, setSearchBox] = useState(false);
+
+  const handleSearchBox = () => {
+    setSearchBox(!searchBox);
+    console.log(searchBox);
+  };
   return (
     <>
-      <nav className="sticky top-0 w-full h-14 bg-neutral-700 px-3 py-1 flex justify-between">
+      <nav className="sticky top-0 w-full h-14 bg-[#032541] px-3 py-1 flex justify-between">
         <div className="flex">
-          <MdOutlineLocalMovies className="text-white w-8 h-8 mt-1 sm:w-11 sm:h-11" />
+          <Icon
+            icon="arcticons:jiocinema"
+            color="white"
+            width="37"
+            height="37"
+          />
           <h1 className="text-white font-bold mt-2 ml-1 text-md sm:text-xl">
-            <Link to={"/"}>Yoe Movie</Link>
+            <Link to={"/"}>Wahyoe Cinema</Link>
           </h1>
-          <MdOutlineLocalMovies className="text-white w-8 h-8 mt-1 sm:w-11 sm:h-11" />
-          <button>
-            <input
-              type="text"
-              placeholder="Search Movie"
-              className="bg-transparent focus:bg-yello-700 text-white"
-            />
-            {/* <Icon icon="bx:search-alt" color="white" width="50" height="50" /> */}
+        </div>
+        <div className="flex">
+          <button className="grid mr-4 text-white font-bold mt-2 ml-1 text-md sm:text-xl">
+            <Link to={"/"}>Home</Link>
+          </button>
+          <button className="mx-4 text-white grid font-bold mt-2 ml-1 text-md sm:text-xl">
+            <Link to={"/Favourite"}>Favourites</Link>
+          </button>
+          <button onClick={() => handleSearchBox()}>
+            {searchBox ? (
+              <Icon
+                icon="codicon:search-stop"
+                color="white"
+                width="37"
+                height="37"
+                hFlip={true}
+              />
+            ) : (
+              <Icon
+                icon="icon-park-outline:search"
+                color="white"
+                width="37"
+                height="37"
+              />
+            )}
           </button>
         </div>
-        <button className="grid text-white font-bold mt-2 ml-1 text-md sm:text-xl">
-          <Link to={"/"}>Home</Link>
-        </button>
-        <button className="text-white grid font-bold mt-2 ml-1 text-md sm:text-xl">
-          <Link to={"/Favourite"}>Favourites</Link>
-        </button>
       </nav>
+      {searchBox ? (
+        <div className="sticky w-full py-2 bg-[#032541] flex justify-between top-14 h-auto">
+          <input
+            className="w-11/12 bg-transparent focus:bg-white/50 focus:text-white text-yellow-400 font-semibold px-2 rounded-full"
+            type="text"
+            placeholder="Search Movie"
+          />
+          <button className="rounded-full w-1/12 bg-gradient-to-r from-[#02B5E3] to-[#1ED5AB] text-white font-semibold text-base">
+            Search
+          </button>
+        </div>
+      ) : null}
     </>
   );
 };
