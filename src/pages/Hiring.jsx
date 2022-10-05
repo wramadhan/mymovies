19a9a6b05f15537ed8c2b8b04d84515031a3c424
wramadhan <wahyuramadhan0549@gmail.com>
2022-10-05 10:53:19 +0700

    perbaikan hiring me

--- src/pages/Hiring.jsx
+++ src/pages/Hiring.jsx
@@ -5,13 +5,18 @@ import NavBar from '../components/NavBar'
 const Hiring = () => {
     const [pageNow, setPageNow] = useState('home')
     return (
-        <div>
-            <Link to={'/hiring'}>
-                <NavBar pageNow={pageNow} />
-                <h1>Sorry</h1>
-                <h1>Comming Soon,page still under construction</h1>
-            </Link>
-        </div>
+        <div className='text-center'>
+            <NavBar pageNow={pageNow} />
+            <h1 className='font-bold'>Sorry,</h1>
+            <h1>This page is still under construction. Please click the button below to be directed to the developer's personal page</h1>
+            <a className='mt-4 flex justify-center'
+                href="https://helloimwahyu.vercel.app/contact"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <button className='border-blue-700 text-blue-700 border-2 px-2 py-[1px] rounded-full font-bold' >Hire me?</button>
+            </a>
+        </div >
     )
 }
 
